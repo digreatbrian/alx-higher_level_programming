76a170697bf293fd011030235d9eaa936c07f313
digreatbrian <digreatbrian@gmail.com>
2024-01-09 12:06:22 -0800

    Initial

---
+++ 0x0A-python-inheritance/runtests.py
@@ -0,0 +1,16 @@
+import os
+
+tests_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "tests")
+
+test_number = 0
+
+for test_file in sorted(os.listdir(tests_dir)):
+	print(f"========= TEST {test_number} ========")
+	print(f'FILE: {test_file}')
+	print('========================== ')
+	with open(os.path.join(tests_dir, test_file), 'r') as fd:
+		f = fd.read()
+		if test_number:
+			exec(f)
+	print(f"========= END TEST ========\n\n")
+	test_number += 1
