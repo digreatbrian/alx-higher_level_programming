b6fa371ba7594b7e1777378aee88af20328f2e84
digreatbrian <digreatbrian@gmail.com>
2024-03-28 13:59:15 -0700

    corrections

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,13 +4,11 @@
 import sys
 import requests
 
-
 if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
     r = requests.get(url)
     commits = r.json()
-    
     try:
         for i in range(10):
             print("{}: {}".format(
