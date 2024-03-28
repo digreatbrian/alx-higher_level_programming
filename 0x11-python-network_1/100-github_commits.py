acc5100b321f55b51f6101bf70a0de69435b024b
digreatbrian <digreatbrian@gmail.com>
2024-03-28 14:30:17 -0700

    Error fixes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Lists the 10 most recent commits on a given GitHub repository
 """
-import sys
 import requests
+from sys import argv
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+    username, repo = argv[1], argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits"
+    url = url.format(username, repo)
     r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
+            sha = commits[i].get("sha")
+            name = commits[i].get("commit").get("author").get("name")
+            print("{}: {}".format(sha, name))
     except IndexError:
         pass
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,9 +6,8 @@ using GET
 import requests
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+    url = "https://intranet.alxswe.com/status"
+    status = requests.get(url).text
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(type(status)))
+    print("\t- content: {}".format(status))
