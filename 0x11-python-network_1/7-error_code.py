23e01b37ad900ddcc8951e65962fc1431a343fd2
digreatbrian <digreatbrian@gmail.com>
2024-03-28 13:20:39 -0700

    Pycodestyle fix

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
-    
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
